5a68bdba8ffc414d6f22e367109a30bbcd2e5645
Andrew Li <andrewyli@airbears2-10-142-33-97.airbears2.1918.berkeley.edu>
2016-09-17 13:20:58 -0700

    email confirmed to work

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request
+import smtplib
+from flaskFeature import main
 app = Flask(__name__)
 
 def parse_csv(csv_location):
@@ -21,3 +23,33 @@ def result():
 def thankyou():
     thankyou_result = request.form
     return render_template("thankyou.html", result=thankyou_result)
+
+class SendMessage:
+
+    def __init__(self, address, password):
+        self.username = address
+        self.password = password
+
+    def send_message(self, email, message, subject = "Text Message", from_address = "The Doctor"):
+
+        fromaddr = self.username
+        toaddrs  = email
+        msg = "\n".join([
+	  "From: " + from_address,
+	  "To: "+email,
+	  "Subject: " + subject,
+	  "",
+	  message
+	  ])
+        username = self.username
+        password = self.password
+        server = smtplib.SMTP('smtp.gmail.com:587')
+        server.ehlo()
+        server.starttls()
+        server.login(username,password)
+        server.sendmail(fromaddr, toaddrs, msg)
+        server.quit()
+        print('sent')
+
+email = SendMessage('ifeelthebern69@gmail.com', 'Potatoes1')
+email.send_message(email.username, "Text Here", "ConnectMe Report", "ConnectMe")
