2097a8e9a01b70d5a9310654ef37f63108fa195e
Andrew Li <andrewyli@airbears2-10-142-33-97.airbears2.1918.berkeley.edu>
2016-09-17 13:10:27 -0700

    three webpage scheme and email setup

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 def parse_csv(csv_location):
@@ -7,8 +7,17 @@ def parse_csv(csv_location):
     df = df[['First Name', 'Last Name', 'E-mail Address', 'Company', 'Job Title']]
     return [df[x].tolist() for x in df]
 
-connections = parse_csv('linkedin_connections_export.csv')
+result = parse_csv('linkedin_connections_export.csv')
 
 @app.route("/")
 def hello_world(name=None):
     return render_template("index.html", name=name)
+
+@app.route('/result', methods = ['POST', 'GET'])
+def result():
+    return render_template("result.html", result=result)
+
+@app.route('/thankyou', methods = ['POST', 'GET'])
+def thankyou():
+    thankyou_result = request.form
+    return render_template("thankyou.html", result=thankyou_result)
